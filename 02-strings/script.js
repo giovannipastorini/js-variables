69954dac88f94f783eabd62772cde87bf9e75a02
giovannipastorini <giovannipastorini9@gmail.com>
2026-02-26 05:22:54 +0100

    complete task 2 strings script

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -10,6 +10,22 @@ console.log(nameOne); /* non usare la variablie 'name' perchè è una variabile 
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
 
+let number=5;
+/* console.log(number); */
+let text=`ciao`;
+/* console.log(text); */
+let bool=true;
+
+console.log(
+    `
+     ${number.toString()}
+     ${text}
+     ${bool.toString()}
+
+    `
+);
+
+
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
